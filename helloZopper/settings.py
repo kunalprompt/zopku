dd519c5b439e3cc1a7fd73f956428c796048a3ad
Kunal Sharma <kunalprompt@gmail.com>
2015-04-18 22:11:34 +0530

    name update - zopku

--- helloZopper/settings.py
+++ helloZopper/settings.py
@@ -47,6 +47,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'djangosecure.middleware.SecurityMiddleware',
 )
 
 ROOT_URLCONF = 'helloZopper.urls'
